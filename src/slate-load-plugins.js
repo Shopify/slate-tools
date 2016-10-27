28acdf017e6161ccc77ea39b59741abe9bc44bfd
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-10-27 18:59:16 -0400

    add tasks back in

--- src/slate-load-plugins.js
+++ src/slate-load-plugins.js
@@ -4,23 +4,40 @@ const join = require('path').join;
 const resolve = require('path').resolve;
 const config = require('./tasks/includes/config');
 
-module.exports = function() {
-  const pattern = ['slate-*', 'slate.*', '@*/slate{-,.}*', '!@shopify/slate-tools'];
-  const scope = ['dependencies', 'devDependencies', 'peerDependencies'];
-  const themePackageJson = require(join(config.themeRoot, 'package.json'));
-  const dependencyNames = scope.reduce((result, prop) => {
-    return result.concat(Object.keys(themePackageJson[prop] || {}));
-  }, []);
-
-  const slatePlugins = uniq(micromatch(dependencyNames, pattern), (name) => {
-    return name;
-  });
-
-  for (const plugin of slatePlugins) {
+function requireDependenciesArray(rootPath, directories) {
+  for (const directory of directories) {
     try {
-      require('require-dir')(resolve(config.themeRoot, `node_modules/${plugin}`));
+      require(resolve(rootPath, `node_modules/${directory}`));
     } catch (err) {
-      console.error(`Could not add ${plugin} to slate tasks`);
+      console.error(`No additional tasks to load from ${directory}.`);
     }
   }
+}
+
+function requireThemeTasks(rootPath) {
+  try {
+    require('require-dir')(join(rootPath, 'tasks'));
+  } catch (err) {
+    console.error('No additional tasks to load from theme.');
+  }
+}
+
+function getSlatePluginNames(packageJson) {
+  const pattern = ['slate-*', 'slate.*', '@*/slate{-,.}*', '!@shopify/slate-tools'];
+  const scope = ['dependencies', 'devDependencies', 'peerDependencies'];
+  const dependencyNames = scope.reduce((result, prop) => {
+    return result.concat(Object.keys(packageJson[prop] || {}));
+  }, []);
+
+  return uniq(micromatch(dependencyNames, pattern), (name) => {
+    return name;
+  });
+}
+
+module.exports = function() {
+  const themePackageJson = require(join(config.themeRoot, 'package.json'));
+  const slatePlugins = getSlatePluginNames(themePackageJson);
+
+  requireThemeTasks(config.themeRoot);
+  requireDependenciesArray(config.themeRoot, slatePlugins);
 };
