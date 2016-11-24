2877ba631fc81df66a37f9b8c1f14f3f6b609778
stevebosworth <steve.bosworth@shopify.com>
2016-11-24 10:42:45 -0500

    updates for clarity

--- src/config.js
+++ src/config.js
@@ -1,25 +1,19 @@
 import {join, normalize} from 'path';
-import existsSync from 'fs';
+import {existsSync} from 'fs';
 import findRoot from 'find-root';
 
 const workingDirectory = process.cwd();
 const currentDirectory = __dirname;
 
 const themeRoot = findRoot(workingDirectory);
-const defaultPath = join(themeRoot, normalize('node_modules/.bin/gulp'));
+const defaultGulpPath = join(themeRoot, normalize('node_modules/.bin/gulp'));
+// Legacy path for older versions of Node.
+const legacyGulpPath = join(themeRoot, normalize('node_modules/@shopify/slate-tools/node_modules/.bin/gulp'));
 
 const config = {
   gulpFile: join(currentDirectory, 'gulpfile.js'),
-  gulp: resolveBinaryPath(defaultPath),
+  gulp: existsSync(defaultGulpPath) ? defaultGulpPath : legacyGulpPath,
   themeRoot,
 };
-
-function resolveBinaryPath(file) {
-  if (existsSync(file)) {
-    return defaultPath;
-  } else {
-    return join(themeRoot, normalize('node_modules/@shopify/slate-tools/node_modules/.bin/gulp'));
-  }
-}
 
 export default config;
